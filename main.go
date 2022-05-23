b60076908c6f65987c7637ebd52294c0b4b2a46e
Sceva Jordan <sceva@Scevas-Air.attlocal.net>
2022-05-23 19:54:31 -0400

    flux

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome! Please hit this page `/qod` API to get the quote of the day."))
+	w.Write([]byte("Welcome! Please hit this pages `/qod` API to get the quote of the day."))
 }
 
 func quoteOfTheDayHandler(client *redis.Client) http.HandlerFunc {
